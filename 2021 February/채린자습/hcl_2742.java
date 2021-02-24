9650e62b64333f37d0c9f5aedbf1fe255a8f432a
chaelin0916 <79526426+chaelin0916@users.noreply.github.com>
2021-02-25 01:48:51 +0900

    Create hcl_2742.java

---
+++ 2021 February/채린자습/hcl_2742.java
@@ -0,0 +1,15 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class forrr {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		for(int i = 0; i < N; N--) {
+			System.out.println(N);
+		
+	}
+
+}
+}
