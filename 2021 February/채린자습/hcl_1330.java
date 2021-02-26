d8dfa7324a5af369c02be6a447c1e061f257b80a
chaelin0916 <79526426+chaelin0916@users.noreply.github.com>
2021-02-26 18:52:13 +0900

    Create hcl_1330.java

---
+++ 2021 February/채린자습/hcl_1330.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Main_2739 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		if(A>B) {
+			System.out.println(">");
+		}
+		if(A<B) {
+			System.out.println("<");
+		}
+		if(A==B) {
+			System.out.println("==");
+		}
+	}
+}
