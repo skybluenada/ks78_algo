4a2c3b07117e1fc370636aa144f612a8cbc7d6a4
kakaru <62199393+skybluenada@users.noreply.github.com>
2021-02-27 21:31:22 +0900

    Update kakaru_1966.java
    for문에서 N번 돌릴때는 33%까지만 맞았는데
    StringTokenizer써서 받은만큼 돌리니깐 정답! (왜그런지는 모르겠다..ㅠㅠ)

--- 2021 February/2021.02.25 스택,큐/kakaru_1966.java
+++ 2021 February/2021.02.25 스택,큐/kakaru_1966.java
@@ -12,42 +12,43 @@ public class Main_1966 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         Scanner sc = new Scanner(System.in);
-        ArrayList<Integer> arr = new ArrayList<Integer>();
         int T = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i <T; i++) {
-            StringTokenizer st2 = new StringTokenizer(br.readLine()," ");
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st2 = new StringTokenizer(br.readLine(), " ");
             int N = Integer.parseInt(st2.nextToken());
             int M = Integer.parseInt(st2.nextToken());
+            ArrayList<Integer> arr = new ArrayList<Integer>();
             Queue<int[]> queue = new LinkedList<int[]>();
             //int N = sc.nextInt();
             //int M = sc.nextInt();
-            for(int j = 0; j < N; j++) {
-                int x = sc.nextInt();
-                if(j == M) {
-                    queue.add(new int[] {x,1});
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int chk = 0;
+            while (st.hasMoreTokens()) {
+                int x = Integer.parseInt(st.nextToken());
+                if (chk == M) {
+                    queue.add(new int[]{x, 1});
+                    arr.add(x);
+                } else {
+                    queue.add(new int[]{x, 0});
                     arr.add(x);
                 }
-                else {
-                    queue.add(new int[] {x,0});
-                    arr.add(x);
-                }
+                chk++;
             }
+
             Collections.sort(arr);
             int cnt = 0;
-            while(!queue.isEmpty()) {
-                if(arr.get(arr.size()-1)==queue.element()[0] && queue.element()[1] == 1) {
-                    arr.remove(arr.size()-1);
+            while (!queue.isEmpty()) {
+                if (arr.get(arr.size() - 1) == queue.element()[0] && queue.element()[1] == 1) {
+                    arr.remove(arr.size() - 1);
                     queue.poll();
                     cnt++;
                     break;
-                }
-                else if(arr.get(arr.size()-1)==queue.element()[0]) {
-                    arr.remove(arr.size()-1);
+                } else if (arr.get(arr.size() - 1) == queue.element()[0]) {
+                    arr.remove(arr.size() - 1);
                     queue.poll();
                     cnt++;
-                }
-                else {
+                } else {
                     queue.add(queue.peek());
                     queue.poll();
                 }
