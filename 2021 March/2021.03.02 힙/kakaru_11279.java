ab2d54dbe5ba7e76db34f357ee4de88cef390772
kakaru <62199393+skybluenada@users.noreply.github.com>
2021-03-02 21:38:54 +0900

    Update kakaru_11279.java
    배열크기 미친... 배열크기때문에 arrayoutofrange..ㅠㅠㅠㅠㅠ

--- 2021 March/2021.03.02 힙/kakaru_11279.java
+++ 2021 March/2021.03.02 힙/kakaru_11279.java
@@ -1,27 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.PriorityQueue;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         int idx = 0;
-        int[] heap = new int[100000];
+        int[] heap = new int[200004];
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < N; i++){
             int input = Integer.parseInt(br.readLine());
             if(input == 0){     // 0입력받음
-                if(idx==0){     // -> 비었으면 0출력,
+                if(idx<=0){     // -> 비었으면 0출력,
                     sb.append("0\n");
                 }
                 else{   // -> 안비었으면 1번 출력하고 1번 삭제과정
                     sb.append(heap[1]+"\n");
-                    heap[1] = heap[idx];
-                    heap[idx] = 0;
-                    for(int j = 1; j <= idx; j*=2){
+                    if(idx != 1){
+                        heap[1] = heap[idx];
+                        heap[idx] = 0;
+                    }
+                    else{
+                        heap[1]= 0;
+                    }
+                    for(int j = 1; j <= idx;){
                         int cmp;
                         if(heap[j*2+1]>heap[j*2]){
                             cmp = j*2+1;
@@ -33,6 +36,10 @@ public class Main {
                             int tmp = heap[j];
                             heap[j] = heap[cmp];
                             heap[cmp] = tmp;
+                            j = cmp;
+                        }
+                        else{
+                            break;
                         }
                     }
                     idx--;
@@ -40,11 +47,15 @@ public class Main {
             }
             else{   // 삽입과정
                 heap[++idx] = input;
-                for(int j = idx; j > 1; j/=2){
+                for(int j = idx; j > 1;){
                     if(heap[j/2] < heap[j]){
                         int tmp = heap[j];
                         heap[j] = heap[j/2];
                         heap[j/2] = tmp;
+                        j /= 2;
+                    }
+                    else{
+                        break;
                     }
                 }
             }
