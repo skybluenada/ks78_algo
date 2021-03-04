d843f6b780d1f7495cc448c4f055d7b0d47d8c9f
kimyb1996 <77041037+kimyb1996@users.noreply.github.com>
2021-03-04 22:53:57 +0900

    Add files via upload

---
+++ 2021 March/2021.03.04 정렬/YB.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        
+        for(int i=0; i<commands.length; i++){
+            int [] arr=new int[commands[i][1]-commands[i][0]+1];
+            int arrIndex=0;
+            
+            for(int j=commands[i][0]-1;j<commands[i][1];j++){
+                arr[arrIndex++]=array[j];
+            }
+            Arrays.sort(arr);
+            answer[i] = arr[commands[i][2]-1];
+        }
+        
+        return answer;
+    }
+}
