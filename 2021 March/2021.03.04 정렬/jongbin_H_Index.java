a594f219fe11d0cea0f6fbe4fd418fc4ee4afa14
jongbini <77082833+jongbini@users.noreply.github.com>
2021-03-06 20:15:11 +0900

    Create jongbin_H_Index.java

---
+++ 2021 March/2021.03.04 정렬/jongbin_H_Index.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+        Arrays.sort(citations);
+	        for(int i = citations.length-1; i >= 0; i--) {
+	        	if(citations[i] > answer) {
+	        		answer++;
+	        		if(citations[i]<= answer) {
+	        			break;
+	        		}
+	        	}
+	        }
+        return answer;
+    }
+}
