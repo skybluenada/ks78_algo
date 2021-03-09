87e56432bc8b96c839721e018e51bd076b362b46
jongbini <77082833+jongbini@users.noreply.github.com>
2021-03-09 19:09:13 +0900

    Update jongbin_10870.java

--- 2021 March/2021.03.09 재귀/jongbin_10870.java
+++ 2021 March/2021.03.09 재귀/jongbin_10870.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 
 public class Test_10870 {
 	public static int f(int x) {
-		if(x <= 1) {
+		if(x < 1) {
+			return 0;
+		}else if(x < 2){
 			return 1;
 		}else {
 			return f(x-1)+f(x-2);
