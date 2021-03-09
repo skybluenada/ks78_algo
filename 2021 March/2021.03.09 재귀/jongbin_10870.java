011a856877f913cdd9eba78936249486d51d3a7f
jongbini <77082833+jongbini@users.noreply.github.com>
2021-03-09 19:09:23 +0900

    Update jongbin_10870.java

--- 2021 March/2021.03.09 재귀/jongbin_10870.java
+++ 2021 March/2021.03.09 재귀/jongbin_10870.java
@@ -15,6 +15,6 @@ public class Test_10870 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		System.out.println(f(n-1));
+		System.out.println(f(n));
 	}
 }
