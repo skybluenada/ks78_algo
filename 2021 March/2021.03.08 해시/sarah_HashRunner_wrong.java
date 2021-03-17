b9f90d3c9a0d496a86014afbd985cb74c101dbed
say3412 <77049729+say3412@users.noreply.github.com>
2021-03-18 02:35:50 +0900

    Update sarah_HashRunner_wrong.java

--- 2021 March/2021.03.08 해시/sarah_HashRunner_wrong.java
+++ 2021 March/2021.03.08 해시/sarah_HashRunner_wrong.java
@@ -1,5 +1,6 @@
 package Programmers;
-// vinko 이클립스에서는 답이 나오지만, 프로그래머스에서는 nikola가 나왔단다.... 왜일까???;;;;
+//이클립스에서는 leo 정답이 나왔는데,
+//프로그래머스에서는 "실행한 결괏값 "kiki"이(가) 기댓값 "leo"와(과) 다릅니다." 라고... 도대체 왜일까?????
 import java.util.Arrays;
 
 public class HashRunner {
@@ -10,11 +11,10 @@ public class HashRunner {
 	        Arrays.sort(participant);
 	        Arrays.sort(completion);
 	        
+	        answer = participant[len-1];
 	        for(int i=0; i<completion.length; i++) {
 	        	if(participant[i] != completion[i]) {
 	        		answer = participant[i];
-	        	}else {
-	        		answer = participant[len-1];
 	        	}
 	        }
 	        
@@ -23,8 +23,8 @@ public class HashRunner {
 	 
 	 public static void main(String[] args) {
 		
-		 String[] participant = {"marina", "josipa", "nikola", "vinko", "filipa"};
-		 String[] completion = {"josipa", "filipa", "marina", "nikola"};
+		 String[] participant = {"leo", "kiki", "eden"};
+		 String[] completion = {"eden", "kiki"};
 		 System.out.println(solution(participant, completion));
 		 
 	}
