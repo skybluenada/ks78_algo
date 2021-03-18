73f386e179fb5ca0adba2178e030f9e75c97c1d6
say3412 <77049729+say3412@users.noreply.github.com>
2021-03-18 19:54:08 +0900

    Update and rename sarah_HashRunner_wrong.java to sarah_HashRunner.java

--- 2021 March/2021.03.08 해시/sarah_HashRunner.java
+++ 2021 March/2021.03.08 해시/sarah_HashRunner.java
@@ -1,6 +1,5 @@
 package Programmers;
-//이클립스에서는 leo 정답이 나왔는데,
-//프로그래머스에서는 "실행한 결괏값 "kiki"이(가) 기댓값 "leo"와(과) 다릅니다." 라고... 도대체 왜일까?????
+
 import java.util.Arrays;
 
 public class HashRunner {
@@ -13,8 +12,9 @@ public class HashRunner {
 	        
 	        answer = participant[len-1];
 	        for(int i=0; i<completion.length; i++) {
-	        	if(participant[i] != completion[i]) {
+	        	if(!participant[i]..equals(completion[i])) {
 	        		answer = participant[i];
+				break;
 	        	}
 	        }
 	        
